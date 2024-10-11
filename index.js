60c1dd19bfa69bf09269a68f6a5daa5a0eb4b405
Ram143shiva <1699733sivaram@gmail.com>
2024-10-11 14:07:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function ClickedGoogle(){
+    console.log("hloooo..")
+}
+function ClickedAmazon(){
+    
+    document.getElementById("about").textContent="Amazon.............................<a href='https://about.google/intl/ALL_in/'> Read more</a>";
+    console.log("txt",document.getElementById("about") )
+}
